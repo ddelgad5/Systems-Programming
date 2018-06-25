4c7f9bb0c464c1cd884f3312484163d410ecb526
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-24 19:38:24 -0500

    Add isnumber function.

--- cache_simulation__optimization/csim.c
+++ cache_simulation__optimization/csim.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 void printHelp();
+bool isNumber(char []);
 
 int main(int argc, char **argv)
 {
   int indexBits, associativity, blockBits;
+  bool verbose = false;
   // printf("Executing program\n");
   if (argc < 0) { // check if there are any arguements
     printf("ERROR\n");
@@ -18,30 +22,37 @@ int main(int argc, char **argv)
       // printf("Reading arguement\n");
       // printf("%s\n", argv[i]);
       if (strcmp(argv[i], "-v") == 0) { // Asking for verbose
-        printf("verbose mode");
+        printf("verbose mode\n");
+        verbose = true;
       }
       else if (strcmp(argv[i], "-h") == 0) { // Asking for help info
-        printf("Asking for help\n");
+        if (verbose) printf("Asking for help\n");
         printHelp();
         exit(0);
       }
       else if (strcmp(argv[i], "-s") == 0) { // Asking for number of index bits
         printf("Setting number of index bits\n");
-        indexBits = *argv[i+1];
-        printf("Index bits: %i\n", indexBits);
-        i++;
+        if (isNumber(argv[i+1])) {
+          indexBits = *argv[i+1];
+          printf("Index bits: %i\n", indexBits);
+          i++;
+        }
       }
       else if (strcmp(argv[i], "-E") == 0) { // Asking for associativity
         printf("Setting associativity\n");
-        associativity = *argv[i+1];
-        printf("Associativity: %i\n", associativity);
-        i++;
+        if (isNumber(argv[i+1])) {
+          associativity = *argv[i+1];
+          printf("Associativity: %i\n", associativity);
+          i++;
+        }
       }
       else if (strcmp(argv[i], "-b") == 0) { // Asking for number of block bits
         printf("Setting number of block bits\n");
-        blockBits = *argv[i+1];
-        printf("Block bits: %i\n", blockBits);
-        i++;
+        if (isNumber(argv[i+1])) {
+          blockBits = *argv[i+1];
+          printf("Block bits: %i\n", blockBits);
+          i++;
+        }
       }
       else if (strcmp(argv[i], "-t") == 0) { // Asking for tracefile
         printf("Asking for tracefile\n");
@@ -66,4 +77,15 @@ void printHelp() {
   printf("-b <b>: Number of block bits (B = 2^b is the block size)\n");
   printf("-t <tracefile>: Name of the valgrind trace to replay\n");
   return;
+}
+
+bool isNumber(char number[]) {
+  int n=0;
+  if (number[0] == '-') { // check for negative
+    n = 1; // start number on second element
+  }
+  for (; number[n] != 0; n++) {
+    if (!isdigit(number[n])) return false;
+  }
+  return true;
 }
